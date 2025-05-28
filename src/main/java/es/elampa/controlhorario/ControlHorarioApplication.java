b6f0783475f6b6f384596399ace9a0ace7898a0e
Irina Medina Sierra <94137035+irinadvallemedina@users.noreply.github.com>
2025-05-28 09:28:20 +0200

    Primer commit

---
+++ src/main/java/es/elampa/controlhorario/ControlHorarioApplication.java
@@ -0,0 +1,13 @@
+package es.elampa.controlhorario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlHorarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlHorarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/elampa/controlhorario/ControlHorarioApplicationTests.java
@@ -0,0 +1,13 @@
+package es.elampa.controlhorario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlHorarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
